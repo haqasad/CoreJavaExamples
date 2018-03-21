8c6fa21587489f729c4048416204e7715c8a169d
haqasad <haq.asad000@gmail.com>
2018-03-21 07:53:06 -0400

    Package Info updated
    Documentation added into Package Info class

--- CoreJavaCodeExamples/src/com/haqasad/main/package-info.java
+++ CoreJavaCodeExamples/src/com/haqasad/main/package-info.java
@@ -1,8 +1,14 @@
-/**
- * 
- */
-/**
- * @author haqasad
- *
- */
 package com.haqasad.main;
+/**
+* <h1>Core Java Code Examples Project</h1>
+* <p>
+* This project contains java code examples for all the features
+* of java language
+* <p>
+* <b>Note:</b> This project will be shared on GitHub Repository
+* <a href="https://github.com/haqasad/CoreJavaExamples">GitHub Repository URI</a? 
+*
+* @author  Asadul Haque
+* @version 1.0
+* @since   2014-03-31
+*/
