d91256aae86805e130f8f3e3d24795a6ce061535
haqasad <haq.asad000@gmail.com>
2018-03-21 07:55:27 -0400

    Main class updated
    Main class updated with documentation and also added to Git Ignore.

--- CoreJavaCodeExamples/src/com/haqasad/main/Main.java
+++ CoreJavaCodeExamples/src/com/haqasad/main/Main.java
@@ -1,7 +1,23 @@
 package com.haqasad.main;
+/**
+* <h1>Implementation Class for Core Java Example Project</h1>
+* This class implements all the examples in Core Java Example
+* Project.
+* <p>
+* <b>Note:</b> This class will be excluded from Git Repository 
+*
+* @author  Asadul Haque
+* @version 1.0
+* @since   03/21/2018
+*/
 
 public class Main {
-
+	/**
+	* This is the main method which makes use of other classes.
+	* @param args Unused.
+	* @return Nothing.
+	* @exception No exceptions.
+	*/
 	public static void main(String[] args) {System.out.println("Hello World!");}
 
 }
